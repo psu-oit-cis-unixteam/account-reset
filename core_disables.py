4139bfea5b358bb4d11a49b7a4309fc738dc62ef
Max <maxp@pdx.edu>
2012-05-17 16:04:25 -0700

    flesh out the fake implementation a bit more

--- core_disables.py
+++ core_disables.py
@@ -20,9 +20,11 @@ class DisableableBase(object):
 class MrFakeDisable(DisableableBase):
 
     def entitlements(self):
-        return ['superadmin', 'ninja', 'samurai']
+        fake_entitlements = ['superadmin', 'ninja', 'samurai']
+        logging.info('%s: uid=%s has entitlements="%s"', self.__class__.__name__, self.uid, str(fake_entitlements))
+        return fake_entitlements
 
     def disable(self, entitlement):
         logging.info("%s is no longer a %s", self.uid, entitlement)
-        # always successful
+        # always successful in the fake implementation
         return True
