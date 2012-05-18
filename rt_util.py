7d09a3bc2b16493bc7d8cbb18cce08da97e18506
Max <maxp@pdx.edu>
2012-05-17 21:30:15 -0700

    fix bad 8 spacers

--- rt_util.py
+++ rt_util.py
@@ -2,16 +2,17 @@ import logging
 import requests
 
 def get(query, credentials, url):
-        q = {'query': query, 'orderby': '-Created', 'format': 's'}
-        rs = requests.post(url, data=credentials, params=q)
-        if rs.ok:
-            return parse_search(rs.text)
-        else:
-            raise Exception("RT Search Failed")
+    q = {'query': query, 'orderby': '-Created', 'format': 's'}
+    rs = requests.post(url, data=credentials, params=q)
+    if rs.ok:
+        return parse_search(rs.text)
+    else:
+        raise Exception("RT Search Failed")
 
 def parse_search(response):
     r = response.split('\n')
     status = r[0]
+    # This is the RT request status, not HTTP status per se
     if '200' not in status:
         raise Exception("RT response parsing failed")
     # we skip the first and last lines in r as they're ''
