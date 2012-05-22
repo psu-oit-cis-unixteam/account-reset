fd04e17efeafb8d031313ad135c07d0e82351037
Max <maxp@pdx.edu>
2012-05-22 11:56:26 -0700

    style

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,14 +1,20 @@
+""" Connect to a local AMQP broker as guest and dynamically load disableable
+    implementations. """
+
 from pkgutil import iter_modules as pkg_iter
 
 import imp
 import logging
 
-def get_implementations(parent_module, fullname=True):
-    _, path, _ = imp.find_module(parent_module)
-    if fullname:
-        implementations = ["{0}.{1}".format(parent_module, name) for _, name, _ in pkg_iter([path])]
-    else:
-        implementations = ["{0}".format(name) for _, name, _ in pkg_iter([path])]
+def get_implementations(parent_package, fullname=True):
+    """Enumerate the modules in parent_package"""
+    _, path, _ = imp.find_module(parent_package)
+    implementations = list()
+    for _, name, _ in pkg_iter([path]):
+        if fullname:
+            implementations.append("{0}.{1}".format(parent_package, name))
+        else:
+            implementations.append("{0}".format(name))
     logging.debug('Loaded disableables: %s', " ".join(implementations))
     return tuple(implementations)
 
